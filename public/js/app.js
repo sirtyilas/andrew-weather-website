56ed91d7d0fd85b0592aa2e7b538a4f52079b3f8
Lubabalo Matyila <sirtyilas84@gmail.com>
2021-07-22 21:15:31 +0200

    Add new information and longitude bug fix

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const messageThree = document.querySelector('#message-3')
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -12,6 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
+    messageThree.textContent = ''
 
     //Used in client side api request
     fetch('/weather?address=' + location).then((response) => {
@@ -19,8 +21,10 @@ weatherForm.addEventListener('submit', (e) => {
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
-                messageOne.textContent = data.location.country
-                messageTwo.textContent = data.current.temperature
+                messageOne.textContent = `The country is ${data.location.country}`
+                messageTwo.textContent = `Current tempreture is ${data.current.temperature}`
+                messageThree.textContent = `The time there is ${data.location.localtime}`
+
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url =  `http://api.weatherstack.com/current?access_key=b7375d667c53b8930501c3ff6e171f90&query=${latitude},-${longitude}`
+    const url =  `http://api.weatherstack.com/current?access_key=b7375d667c53b8930501c3ff6e171f90&query=${latitude},${longitude}`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
